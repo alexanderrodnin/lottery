d6d5db642dec8d87e0e0a00c3ae7151226f618f7
alexanderrodnin <alexander.rodnin@gmail.com>
2022-05-02 18:39:18 +0400

    Lottery UI with stubs implemented

--- src/App.js
+++ src/App.js
@@ -1,23 +1,112 @@
-import logo from './media/images/logo.svg';
+import React from 'react';
 import './styles/App.css';
+
+class Lottery extends React.Component {
+
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      managerAddress: "0xMANAGER_ADDRESS",
+      competitorAddresses: [],
+      winnerAddress: null,
+      bank: 0,
+      currentAmount: ""
+    }  
+
+    this.handleCurrentAmountChange = this.handleCurrentAmountChange.bind(this)
+    this.handleAmmountSubmit = this.handleAmmountSubmit.bind(this)
+    this.handlePickWinner = this.handlePickWinner.bind(this)
+  }
+
+  handleCurrentAmountChange(event) {
+    this.setState(
+      {
+        ...this.state,
+        ...{
+          currentAmount: event.target.value
+        }
+      }
+    )
+  }
+
+  handleAmmountSubmit(event) {
+    const currentAmountFloat = parseFloat(this.state.currentAmount)
+    if (isNaN(currentAmountFloat)) {
+      alert("Error:\n\t value is not a Number")
+      return;
+    }
+
+    const newAddresses = this.state.competitorAddresses
+    newAddresses.push(parseInt(Math.random()*1000000000).toString())
+    this.setState(
+      {
+        ...this.state,
+        ...{
+          competitorAddresses: newAddresses,
+          bank: this.state.bank + currentAmountFloat,
+          currentAmount: "",
+          winnerAddress: null
+        }
+      }
+    )
+  }
+
+  handlePickWinner(event) {
+    const winnerAddressNew = this.state.competitorAddresses[Math.floor(Math.random() * this.state.competitorAddresses.length)]
+    this.setState(
+      {
+        ...this.state,
+        ...{
+          winnerAddress: winnerAddressNew,
+          competitorAddresses: [],
+          bank: 0,
+          currentAmount: ""
+        }
+      }
+    )
+  }
+
+  render() {
+    return (
+      <div>
+        <h1 className="App-header">
+          Lottery contract!
+        </h1>
+        <div>
+          This contract is managed by <strong>{this.state.managerAddress}</strong><br />
+          There are currently <strong>{this.state.competitorAddresses.length}</strong> people entered, competing to win <strong>{this.state.bank}</strong> ether!
+        </div>
+        <h2>
+          Want to try your luck?
+        </h2>
+        <div>
+          Amount of ether to enter:&nbsp;
+          <input type='text' value = {this.state.currentAmount} onChange = {this.handleCurrentAmountChange} />&nbsp;
+          <button type='button' onClick={this.handleAmmountSubmit}>Enter</button>
+        </div>
+        <h2>
+          Time to pick winner?
+        </h2>
+        <button type='button' onClick={this.handlePickWinner}>Pick Winner</button><br />
+        {
+          this.state.winnerAddress && 
+          <>
+          <strong>{this.state.winnerAddress}</strong> has won!
+          </>
+        }
+        
+      </div>
+    )
+  }
+
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Lottery />
     </div>
   );
 }
